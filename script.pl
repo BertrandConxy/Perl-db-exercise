f96995e76a035ad5f32641e933746b5142d7ed39
Bertrand Mutangana <bertrand@CHOICEs-MacBook-Pro-2.local>
2024-01-29 20:17:45 +0200

    program that reads a file and retrieves info

--- script.pl
+++ script.pl
@@ -2,7 +2,29 @@
 die "Required arguments not found\n" unless @ARGV == 2;
 my $file = $ARGV[0];
 my $site_name = $ARGV[1];
+my $found;
 
 open FH, "<$file" or die "can't open $file. $! \n";
 
-my ($name, $location, $mapref, $type, $description)
+my ($name, $location, $mapref, $type, $description);
+
+while (FH) {
+    chop;
+    ($name, $location, $mapref, $type, $description) = split(/:/, $_);
+
+    if($name eq $site_name) {
+        $found = $.;
+        last;
+    }
+
+}
+if($found) {
+    print "location: $name on the line $found\n";
+    print "information on $name($type)\n";
+    print "Map: $mapref";
+    print "Description: $description";
+}
+
+close FH;
+
+exit;
